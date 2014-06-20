8243da2b7d37933d612f3cdf1fd46eda4fd6366c
kennytrionfo <kentrionfo@gmail.com>
2014-06-19 21:25:27 -0600

    todays notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619200641) do
+ActiveRecord::Schema.define(version: 20140619233152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "hits", force: true do |t|
+    t.string   "message"
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "hits", ["user_id"], name: "index_hits_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
